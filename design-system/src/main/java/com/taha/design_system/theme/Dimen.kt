9ab42ffbac6eef3abf7d6aeb880f92cbf5ad640f
tsouilah <taha.souilah@gmail.com>
2025-03-17 13:49:35 +0100

    chore: Initial project setup with core modules

---
+++ design-system/src/main/java/com/taha/design_system/theme/Dimen.kt
@@ -0,0 +1,12 @@
+package com.taha.design_system.theme
+
+import androidx.compose.ui.unit.dp
+
+val SmallPadding = 8.dp
+val NormalPadding = 12.dp
+val LargePadding = 16.dp
+
+val LoaderSize = 48.dp
+
+val CardRoundSize = 5.dp
+val CardPadding = NormalPadding
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "public-toilets"
+include(":app")
+include(":data")
+include(":domain")
+include(":design-system")
