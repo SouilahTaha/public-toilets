a88a6b6faa9af707a92dc870e0de37f6749112d7
SouilahTaha <souilahtaha9@gmail.com>
2025-03-19 17:20:43 +0100

    test: implement tests for LocationExtensions

--- app/src/main/java/com/taha/publictoilets/extenstions/Location.kt
+++ app/src/main/java/com/taha/publictoilets/extenstions/Location.kt
@@ -4,7 +4,7 @@ import android.location.Location
 import com.google.android.gms.maps.model.LatLng
 import kotlin.math.round
 
-internal fun Location.toLatLng() = LatLng(this.latitude, this.longitude)//todo add test
+internal fun Location.toLatLng() = LatLng(this.latitude, this.longitude)
 
 internal fun LatLng.calculateDistance(location: LatLng): Float {
   val results = FloatArray(1)
@@ -15,7 +15,6 @@ internal fun LatLng.calculateDistance(location: LatLng): Float {
     /* endLongitude = */ location.longitude,
     /* results = */ results
   )
-
   val distanceInKm = results[0] / 1000
   return round(distanceInKm * 10) / 10
 }
